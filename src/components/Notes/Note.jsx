8a8ea650298f352621b41c5ffa7759ac095b5e74
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2024-12-07 21:23:45 +0530

    Updated frontend

--- src/components/Notes/Note.jsx
+++ src/components/Notes/Note.jsx
@@ -222,7 +222,6 @@ export function BasicTabs() {
           {archivedNotes.map((note) => (
             <div className="note-item" key={note.id}>
               <h2>{note.title}</h2>
-              
               <p>{note.content}</p>
               <AutorenewIcon onClick={() => revokeArchive(note.id)} />
             </div>
